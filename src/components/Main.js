45511d0702571ed0e3fc6ba913e45d1c9ede8dcc
Abraham <abrahamsmith1998@gmail.com>
2022-11-11 09:56:57 -0500

    condensed navbar code

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,10 +14,7 @@ function Main () {
         }
     }
     const [current, setCurrent] = useState(About);
-    const [aboutTab, setAboutTab] = useState(style.active);
-    const [portfolioTab, setPortfolioTab] = useState(style.inactive);
-    const [contactTab, setContactTab] = useState(style.inactive);
-    const [resumeTab, setResumeTab] = useState(style.inactive);
+    const [currentPage, setCurrentPage] = useState('about');
 
     function handleClick(e) {
         const target = (e.target.innerHTML);
@@ -25,36 +22,28 @@ function Main () {
         switch (target) {
             case 'About Me':
                 setCurrent(About);
-                setAboutTab(style.active);
-                setPortfolioTab(style.inactive);
-                setContactTab(style.inactive);
-                setResumeTab(style.inactive);
+                setCurrentPage('about');
                 break;
             case 'Portfolio':
                 setCurrent(Portfolio);
-                setPortfolioTab(style.active)
-                setAboutTab(style.inactive);
-                setContactTab(style.inactive);
-                setResumeTab(style.inactive);
+                setCurrentPage('portfolio');
                 break;
             case 'Contact':
                 setCurrent(Contact);
-                setContactTab(style.active)
-                setAboutTab(style.inactive);
-                setPortfolioTab(style.inactive);
-                setResumeTab(style.inactive);
+                setCurrentPage('contact');
                 break;
             case 'Resume':
                 setCurrent(Resume);
-                setResumeTab(style.active)
-                setAboutTab(style.inactive);
-                setPortfolioTab(style.inactive);
-                setContactTab(style.inactive);
+                setCurrentPage('resume');
                 break;
             default:
                 break;
         }
 
+    }
+
+    function handleMenuClick () {
+        
     }
 
     return (
@@ -65,14 +54,14 @@ function Main () {
                 <h1 className="col-4 mx-3">Abraham Smith</h1>
 
                 <div className="col-6 d-md-none d-flex justify-content-end px-5">
-                    <button type="button" className="btn btn-light">☰ Menu</button>
+                    <button onClick={() => handleMenuClick} type="button" className="btn btn-light">☰ Menu</button>
                 </div>
                 
                 <div className="col-6 navi d-none d-md-flex float-right justify-content-around">
-                    <a href="_#" onClick={handleClick} id='about' className="navi-item text-center text-white border-bottom border-white"><span style={aboutTab} className="nav-text">About Me</span></a>
-                    <a href="_#" onClick={handleClick} id='portfolio' className="navi-item text-center text-white border-bottom border-white"><span style={portfolioTab} className="nav-text ">Portfolio</span></a>
-                    <a href="_#" onClick={handleClick} id='contact' className="navi-item text-center text-white border-bottom border-white"><span style={contactTab} className="nav-text ">Contact</span></a>
-                    <a href="_#" onClick={handleClick} id='resume' className="navi-item text-center text-white border-bottom border-white"><span style={resumeTab} className="nav-text ">Resume</span></a>
+                    <a href="_#" onClick={handleClick} id='about' className="navi-item text-center text-white border-bottom border-white"><span style={currentPage === 'about' ? style.active : style.inactive} className="nav-text">About Me</span></a>
+                    <a href="_#" onClick={handleClick} id='portfolio' className="navi-item text-center text-white border-bottom border-white"><span style={currentPage === 'portfolio' ? style.active : style.inactive} className="nav-text ">Portfolio</span></a>
+                    <a href="_#" onClick={handleClick} id='contact' className="navi-item text-center text-white border-bottom border-white"><span style={currentPage === 'contact' ? style.active : style.inactive} className="nav-text ">Contact</span></a>
+                    <a href="_#" onClick={handleClick} id='resume' className="navi-item text-center text-white border-bottom border-white"><span style={currentPage === 'resume' ? style.active : style.inactive} className="nav-text ">Resume</span></a>
                 </div>
             </nav> 
         </header>
